d9ba855598bb1ef1908c976f9bb9e61309ff134e
Aliaksei Barysevich <barysevichaliaksei@yandex.by>
2023-01-28 15:29:42 +0300

    feat: signuo and login requests validated

--- app.js
+++ app.js
@@ -4,8 +4,10 @@ const cors = require('cors');
 require('dotenv').config();
 const rateLimit = require('express-rate-limit');
 const helmet = require('helmet');
-// const { errors } = require('celebrate');
+const { celebrate, Joi, errors } = require('celebrate');
 const { login, createUser } = require('./controllers/users');
+const { validateUrl } = require('./utils/validateUrl');
+const NotFoundError = require('./errors/not-found-err');
 
 const app = express();
 const { PORT = 3000 } = process.env;
@@ -23,10 +25,24 @@ app.use(helmet());
 app.use(limiter);
 app.use(cors());
 app.use('/', routes);
-app.post('/signin', login);
-app.post('/signup', express.json(), createUser);
-app.use('*', (req, res) => res.status(404).send({ message: 'Страница не найдена' }));
-// app.use(errors());
+app.post('/signin', celebrate({
+  body: Joi.object().keys({
+    email: Joi.string().required().email(),
+    password: Joi.string().required(),
+  }),
+}), login);
+app.post('/signup', celebrate({
+  body: Joi.object().keys({
+    email: Joi.string().email().required(),
+    password: Joi.string().required(),
+    name: Joi.string().min(2).max(30),
+    about: Joi.string().min(2).max(30),
+    avatar: Joi.string().custom(validateUrl),
+  }),
+}), express.json(), createUser);
+app.use('*', (req, res, next) => next(new NotFoundError('Страница не найдена')));
+
+app.use(errors());
 // eslint-disable-next-line no-unused-vars
 app.use((err, req, res, next) => {
   const { statusCode = 500, message } = err;
