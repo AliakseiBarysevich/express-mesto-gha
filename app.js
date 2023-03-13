9b39dbb120dd4ab74dceefafe337e645908965a4
Aliaksei Barysevich <barysevichaliaksei@yandex.by>
2023-03-13 11:32:35 +0300

    fix: incorrect route moved to routes file; err object removed from the server response

--- app.js
+++ app.js
@@ -5,7 +5,6 @@ require('dotenv').config();
 const rateLimit = require('express-rate-limit');
 const helmet = require('helmet');
 const { errors } = require('celebrate');
-const NotFoundError = require('./errors/not-found-err');
 
 const app = express();
 const { PORT = 3000 } = process.env;
@@ -24,21 +23,19 @@ app.use(limiter);
 app.use(cors());
 app.use(express.json());
 app.use('/', routes);
-app.use('*', (req, res, next) => next(new NotFoundError('Страница не найдена')));
 
 app.use(errors());
-// eslint-disable-next-line no-unused-vars
 app.use((err, req, res, next) => {
   const { statusCode = 500, message } = err;
 
   res
     .status(statusCode)
     .send({
-      err,
       message: statusCode === 500
         ? 'На сервере произошла ошибка'
         : message,
     });
+  next();
 });
 
 app.listen(PORT);
--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,7 @@ const { celebrate, Joi } = require('celebrate');
 const { login, createUser } = require('../controllers/users');
 const { validateUrl } = require('../utils/validateUrl');
 const { auth } = require('../middlewares/auth');
+const NotFoundError = require('../errors/not-found-err');
 
 const routes = express.Router();
 const usersRoutes = require('./users');
@@ -27,5 +28,6 @@ routes.post('/signin', celebrate({
 
 routes.use('/users', auth, usersRoutes);
 routes.use('/cards', auth, cardsRoutes);
+routes.use('*', (req, res, next) => next(new NotFoundError('Запрашиваемая страница не найдена')));
 
 module.exports = routes;
